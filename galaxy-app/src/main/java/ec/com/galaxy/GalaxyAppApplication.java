79b3b9a6df1af06f2697f0144874aa3f294844c4
abenalcazar <abenalcazar@pc-smxk21.KRUGER.COM.EC>
2020-11-10 14:14:24 -0500

    Initial commit in eclipse

---
+++ galaxy-app/src/main/java/ec/com/galaxy/GalaxyAppApplication.java
@@ -0,0 +1,13 @@
+package ec.com.galaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyAppApplication.class, args);
+	}
+
+}
